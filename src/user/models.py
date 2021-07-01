0102861bd238b54f17ae86a4787831d0a935075f
w0rng <antonabramov2000@gmail.com>
2021-07-01 17:18:28 +0700

    add signal for generate token

--- src/user/models.py
+++ src/user/models.py
@@ -1,5 +1,8 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 
 class User(AbstractUser):
@@ -11,3 +14,9 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.get_full_name()
+
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)

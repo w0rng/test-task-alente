520ce45bf71460c2933dde5ca5e5845c57acc515
w0rng <antonabramov2000@gmail.com>
2021-07-01 17:57:14 +0700

    create view for event

--- src/core/api.py
+++ src/core/api.py
@@ -1,6 +1,8 @@
 from rest_framework.routers import SimpleRouter
+from events import views as event_views
 
 router = SimpleRouter()
+router.register('events', event_views.EventViewSet)
 
 
 __all__ = [
--- src/events/views.py
+++ src/events/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from . import models, serializers, permissions
+
+
+class EventViewSet(ModelViewSet):
+    queryset = models.Event.objects.filter(start_date__gt=datetime.now())
+    serializer_class = serializers.EventSerializer
+    permission_classes = [permissions.IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.validated_data['author'] = self.request.user
+        serializer.save()

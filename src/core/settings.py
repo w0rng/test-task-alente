cb60dc8db4debff4abc709f50806ed9ffe46ab47
w0rng <antonabramov2000@gmail.com>
2021-07-01 18:32:04 +0700

    add pagination

--- src/core/settings.py
+++ src/core/settings.py
@@ -97,6 +97,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 30,
 }
 
 LANGUAGE_CODE = 'ru-ru'

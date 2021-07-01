e86feb5c592a0a88233998253d1abd2ae3b36b8a
w0rng <antonabramov2000@gmail.com>
2021-07-01 15:12:35 +0700

    sending emails

--- src/events/models.py
+++ src/events/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from anymail.exceptions import AnymailRequestsAPIError
 from django.conf import settings
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
@@ -23,20 +24,39 @@ class Event(models.Model):
         verbose_name_plural = 'Мероприятия'
 
 
-class Request(models.Model):
+class ForeignUserEvent(models.Model):
     date = models.DateTimeField('Дата создания', default=datetime.now, editable=False)
     event = models.ForeignKey(Event, on_delete=models.CASCADE, verbose_name='Мероприятие')
     participant = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Участник')
+
+    TITLE_MESSAGE = 'Новое событие'
+    TEXT_MESSAGE = 'С вашим мероприятием {0} что-то случилось'
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        try:
+            self.event.author.email_user(self.TITLE_MESSAGE, self.TEXT_MESSAGE.format(self.event.name))
+        except AnymailRequestsAPIError:
+            pass
+
+
+class Request(ForeignUserEvent):
+    TITLE_MESSAGE = 'Новая заявка'
+    TEXT_MESSAGE = 'На ваше мероприятие {0} зарегистрировался новый пользователь'
 
     class Meta:
         verbose_name = 'Заявка'
         verbose_name_plural = 'Заявки'
 
 
-class Feedback(models.Model):
-    date = models.DateTimeField('Дата создания', default=datetime.now, editable=False)
-    event = models.ForeignKey(Event, on_delete=models.CASCADE, verbose_name='Мероприятие')
-    participant = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Участник')
+class Feedback(ForeignUserEvent):
+    TITLE_MESSAGE = 'Новый отзыв'
+    TEXT_MESSAGE = 'Вашему мероприятию {0} оставили новый отзыв'
+
     file = models.FileField('Дополнительный материал', blank=True, upload_to='feedback')
     rating = models.IntegerField(
         verbose_name='Рейтинг',

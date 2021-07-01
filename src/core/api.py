e0eaba6f527c7badcdc68165198099a261c16cef
w0rng <antonabramov2000@gmail.com>
2021-07-01 17:18:38 +0700

    add router

---
+++ src/core/api.py
@@ -0,0 +1,8 @@
+from rest_framework.routers import SimpleRouter
+
+router = SimpleRouter()
+
+
+__all__ = [
+    router,
+]
--- src/core/urls.py
+++ src/core/urls.py
@@ -1,12 +1,16 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.conf.urls.static import static
-
+from .api import router
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('v1/auth', obtain_auth_token),
+    path('v1/', include(router.urls)),
 ]
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 

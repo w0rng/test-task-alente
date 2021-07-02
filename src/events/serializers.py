49d7304a22b30754cdf33f84dc69d6b9ccb1994c
w0rng <antonabramov2000@gmail.com>
2021-07-02 16:48:50 +0700

    add num_participants and avg_rate in serializer

--- src/events/serializers.py
+++ src/events/serializers.py
@@ -1,15 +1,16 @@
 from rest_framework import serializers
-from rest_framework.validators import UniqueTogetherValidator
 
 from . import models
 
 
 class EventSerializer(serializers.ModelSerializer):
     user = serializers.CharField(read_only=True)
+    num_participants = serializers.IntegerField(read_only=True)
+    avg_rate = serializers.FloatField(read_only=True, default=0)
 
     class Meta:
         model = models.Event
-        fields = '__all__'
+        fields = ('name', 'description', 'start_date', 'user', 'num_participants', 'avg_rate')
 
 
 class RequestSerializer(serializers.ModelSerializer):
--- src/events/views.py
+++ src/events/views.py
@@ -1,7 +1,9 @@
+from django.db.models import Count, Avg
+from django.db.models.functions import Coalesce
 from django.utils import timezone
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 
@@ -9,13 +11,18 @@ from . import models, serializers, permissions, services
 
 
 class EventViewSet(ModelViewSet):
-    queryset = models.Event.objects.filter(start_date__gt=timezone.now())
+    queryset = models.Event.objects.filter(
+        start_date__gt=timezone.now(),
+    ).annotate(
+        num_participants=Count('request', distinct=True),
+        avg_rate=Coalesce(Avg('feedback__rating'), 0.0)
+    )
     serializer_class = serializers.EventSerializer
     permission_classes = [permissions.IsAuthorOrReadOnly, permissions.IsOwnerOrReadOnly]
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filter_fields = ['name']
     search_fields = ['name', 'start_date', 'user__username']
-    ordering_fields = ['name', 'start_date', 'user__username']
+    ordering_fields = ['name', 'start_date', 'user__username', 'num_participants', 'avg_rate']
 
     def perform_create(self, serializer):
         serializer.validated_data['user'] = self.request.user

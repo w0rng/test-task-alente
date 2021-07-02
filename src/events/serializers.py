8a953d512db716221417321b420d4dfb72b10103
w0rng <antonabramov2000@gmail.com>
2021-07-02 17:46:39 +0700

    create serializer for get detail info about Event and get Feedback

--- src/events/serializers.py
+++ src/events/serializers.py
@@ -4,13 +4,13 @@ from . import models
 
 
 class EventSerializer(serializers.ModelSerializer):
-    user = serializers.CharField(read_only=True)
+    user = serializers.CharField(read_only=True, default=serializers.CurrentUserDefault())
     num_participants = serializers.IntegerField(read_only=True)
     avg_rate = serializers.FloatField(read_only=True, default=0)
 
     class Meta:
         model = models.Event
-        fields = ('name', 'description', 'start_date', 'user', 'num_participants', 'avg_rate')
+        fields = ('name', 'start_date', 'user', 'num_participants', 'avg_rate')
 
 
 class RequestSerializer(serializers.ModelSerializer):
@@ -19,3 +19,21 @@ class RequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Request
         fields = '__all__'
+
+
+class FeedbackSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True, default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = models.Feedback
+        fields = '__all__'
+
+
+class EventDetailSerializer(serializers.ModelSerializer):
+    feedback_set = FeedbackSerializer(many=True, read_only=True)
+    num_participants = serializers.IntegerField(read_only=True)
+    avg_rate = serializers.FloatField(read_only=True, default=0)
+
+    class Meta:
+        model = models.Event
+        fields = ('name', 'description', 'start_date', 'user', 'num_participants', 'avg_rate', 'feedback_set')
